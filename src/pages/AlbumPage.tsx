c07317f413458766fcce4c793b404a9d1f794d80
fennec01 <shinarix5@gmail.com>
2025-04-29 00:32:26 +0200

    Fix import

--- src/pages/AlbumPage.tsx
+++ src/pages/AlbumPage.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@/components/ui/button";
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { useTheme } from "@/hooks/useTheme";
