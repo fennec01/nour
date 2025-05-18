e975572257f276c3be4f8200436b2b7a27a93fd2
fennec01 <shinarix5@gmail.com>
2025-05-19 00:41:34 +0200

    Update image URLs for warsh + hafs

--- src/pages/SurahPage.tsx
+++ src/pages/SurahPage.tsx
@@ -120,8 +120,8 @@ export function SurahPage() {
                 <img
                 src={
                   reciterQuran[selectedReciter] === 'warsh'
-                  ? `https://cdn.jsdelivr.net/gh/fennec01/nour@gh-pages/img/warsh/${surahId}/${part}.jpg`
-                  : `https://ik.imagekit.io/hefz/quran/${surah?.imagesStartIndex + index}.webp`
+                  ? `https://hefzmoyaser.net/images/quran2/${surah?.imagesStartIndex + index}.webp`
+                  : `https://hefzmoyaser.net/images/quran/${surah?.imagesStartIndex + index}.webp`
                 }
                 alt={`Preview for part ${part}`}
                 className={`max-w-full h-auto ${isDark ? 'invert' : ''}`}
