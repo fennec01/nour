5e53a0238bdc0096cce0ae70ab44d48255786694
fennec01 <shinarix5@gmail.com>
2025-11-30 23:03:14 +0100

    Add surat Al-Mu'minun (23)

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,10 +1,24 @@
 export const dataPro = [
     {
+        id: 12,
+        name: "يوسف",
+        parts: 14,
+        reciters: ["humaid","afasi"],
+        imagesStartIndex: 235
+    },
+    {
         id: 18,
         name: "الكهف",
         parts: 12,
         reciters: ["humaid"],
         imagesStartIndex: 293
+    },
+    {
+        id: 23,
+        name: "المؤمنون",
+        parts: 3,
+        reciters: ["humaid"],
+        imagesStartIndex: 342
     },
     {
         id: 31,
@@ -19,12 +33,5 @@ export const dataPro = [
         parts: 3,
         reciters: ["hamza","yacin","afasi"],
         imagesStartIndex: 526
-    },
-    {
-        id: 12,
-        name: "يوسف",
-        parts: 14,
-        reciters: ["humaid","afasi"],
-        imagesStartIndex: 235
     }
 ];
